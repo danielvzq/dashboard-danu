35507c33a12997779ad182b7073afa89e8924666
Diego Roca <diegoroca05@outlook.com>
2026-05-23 21:14:39 -0600

    configuración de entorno y estructura inicial con shadcn

--- components/card.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def chart_card(title, content):
-    st.markdown('<div class="chart-card">', unsafe_allow_html=True)
-    st.subheader(title)
-    st.write(content)
-    st.markdown('</div>', unsafe_allow_html=True)
